f53601a5781fdc097b257b43b28cfb336f7d581a
MarcusIversen <marcus.iversen@lego.com>
2024-09-17 12:46:58 +0200

    refactored controllers

--- BasicAuthAPI/Controllers/CommentController.cs
+++ BasicAuthAPI/Controllers/CommentController.cs
@@ -1,103 +1,22 @@
-﻿using BasicAuthAPI.Core.Entities;
+using BasicAuthAPI.Core.Entities;
 using BasicAuthAPI.Database;
 using BasicAuthAPI.DTOs.NewsDTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BasicAuthAPI.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class NewsController : ControllerBase
+public class CommentController: ControllerBase
 {
     private readonly DatabaseContext _databaseContext;
     
-    public NewsController(DatabaseContext databaseContext)
+    public CommentController(DatabaseContext databaseContext)
     {
         _databaseContext = databaseContext;
     }
-
-    [HttpGet]
-    [Route("/Articles/GetAllArticles")]
-    public IEnumerable<ArticleDto> GetAllArticles()
-    {
-        return _databaseContext.Articles.Include(x => x.Author).Select(ArticleDto.FromEntity);
-    }
-
-    [HttpGet]
-    [Route("/Articles/{articleId}")]
-    public ArticleDto? GetArticleById([FromRoute] int articleId)
-    {
-        return _databaseContext
-            .Articles.Include(x => x.Author)
-            .Where(x => x.Id == articleId)
-            .Select(ArticleDto.FromEntity)
-            .SingleOrDefault();
-    }
-
-    [HttpDelete]
-    [Route("/Articles/{articleId}")]
-    public IActionResult DeleteArticleById([FromRoute] int articleId)
-    {
-        // Fetch the article from the database
-        var article = _databaseContext.Articles
-            .Include(x => x.Author)
-            .SingleOrDefault(x => x.Id == articleId);
-
-        // Check if the article exists
-        if (article == null)
-        {
-            return NotFound("Article not found");
-        }
-
-        // Remove the article
-        _databaseContext.Articles.Remove(article);
-        _databaseContext.SaveChanges();
-
-        // Return a success response
-        return Ok(ArticleDto.FromEntity(article));
-    }
-
-
-    [HttpPut]
-    [Route("/Articles/{articleId}")]
-    public ArticleDto EditArticle([FromBody] ArticleFormDto dto, [FromRoute] int articleId)
-    {
-        var userName = HttpContext.User.Identity?.Name;
-        var entity = _databaseContext
-            .Articles
-            .Include(x => x.Author)
-            .Single(x => x.Id == articleId);
-        entity.Title = dto.Title;
-        entity.Content = dto.Content;
-        var updated = _databaseContext.Articles.Update(entity).Entity;
-        _databaseContext.SaveChanges();
-        return ArticleDto.FromEntity(updated);
-    }
-
-    [HttpPost]
-    [Route("/Articles/CreateArticle")]
-    public ArticleDto CreateArticle([FromBody] ArticleFormDto formDto)
-    {
-        var userName = HttpContext.User.Identity?.Name;
-        var author = _databaseContext.Users.Single(x => x.UserName == userName);
-        var entity = new Article
-        {
-            Title = formDto.Title,
-            Content = formDto.Content,
-            Author = author,
-            CreatedAt = DateTime.Now
-        };
-        var created = _databaseContext.Articles.Add(entity).Entity;
-        _databaseContext.SaveChanges();
-        return ArticleDto.FromEntity(created);
-    }
     
-    /**
-     * -------------- Below is for comments -------------
-     * (This should really be split into 2 controllers.
-     */
-    
+    // No Auth because all roles are allowed to see comments
     [HttpGet]
     [Route("/Comments/GetCommentsOnArticle")]
     public IEnumerable<CommentDto> Get([FromQuery] int? articleId)
@@ -109,6 +28,7 @@ public class NewsController : ControllerBase
     }
     
     [HttpGet]
+    [Authorize]
     [Route("/Comments/{commentId}")]
     public CommentDto? GetById(int commentId)
     {
@@ -120,6 +40,7 @@ public class NewsController : ControllerBase
 
 
     [HttpPost]
+    [Authorize]
     [Route("/Comments/CreateComment")]
     public CommentDto Post([FromBody] CommentFormDto dto)
     {
@@ -138,6 +59,7 @@ public class NewsController : ControllerBase
     }
 
     [HttpDelete]
+    [Authorize]
     [Route("/Comments/{commentId}")]
     public IActionResult DeleteComment([FromRoute] int commentId)
     {
@@ -161,6 +83,7 @@ public class NewsController : ControllerBase
     }
 
     [HttpPut]
+    [Authorize]
     [Route("/Comments/{commentId}")]
     public CommentDto Put(int id, [FromBody] CommentFormDto dto)
     {
